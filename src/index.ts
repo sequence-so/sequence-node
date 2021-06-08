1ffa92b78e2b08f59fc0393d9579c1adc2618f14
Helson <helsont@gmail.com>
2021-06-08 16:18:57 -0700

    fixed typescript files not getting included in npm output

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import Sequence from './sequence';
-export { SequenceOptions, Track, Identify, EventContext, SequenceEventType, CallbackFunction } from './types';
+export {
+  SequenceOptions,
+  Track,
+  Identify,
+  EventContext,
+  SequenceEventType,
+  CallbackFunction,
+  EventPayload,
+} from './types';
 export default Sequence;
